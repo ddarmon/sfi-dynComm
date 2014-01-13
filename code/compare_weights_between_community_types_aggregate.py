ebf961962a29983d731a377824d8d5b82bdfedf1
David Darmon <ddarmon@math.umd.edu>
2014-01-12 22:47:57 -0500

    Added my attempt at the abstract.

--- code/compare_weights_between_community_types_aggregate.py
+++ code/compare_weights_between_community_types_aggregate.py
@@ -7,38 +7,39 @@ import numpy
 
 label_type = 'struc'
 weight_type = 'hashtag'
+for weight_type in ['TE4', 'mention-retweet', 'hashtag']:
+	for label_type in ['struc', 'TE4', 'mention-retweet', 'hashtag']:
+		print 'Using the {}/{} pairing.'.format(label_type, weight_type)
 
-print 'Using the {}/{} pairing.'.format(label_type, weight_type)
+		ratio_e_to_i = []
+		ratio_i_to_e = []
 
-ratio_e_to_i = []
-ratio_i_to_e = []
+		for comm_rank in range(100):
+			file_prefix = 'comm{}_labels-{}_weights-{}'.format(comm_rank, label_type, weight_type)
 
-for comm_rank in range(100):
-	file_prefix = 'comm{}_labels-{}_weights-{}'.format(comm_rank, label_type, weight_type)
+			i_to_i = numpy.loadtxt('../data/edges-by-type/{}_i-to-i.dat'.format(file_prefix))
+			e_to_i = numpy.loadtxt('../data/edges-by-type/{}_e-to-i.dat'.format(file_prefix))
+			i_to_e = numpy.loadtxt('../data/edges-by-type/{}_i-to-e.dat'.format(file_prefix))
 
-	i_to_i = numpy.loadtxt('../data/edges-by-type/{}_i-to-i.dat'.format(file_prefix))
-	e_to_i = numpy.loadtxt('../data/edges-by-type/{}_e-to-i.dat'.format(file_prefix))
-	i_to_e = numpy.loadtxt('../data/edges-by-type/{}_i-to-e.dat'.format(file_prefix))
+			# print 'The i-to-i, e-to-i, and i-to-e medians are {}, {}, {}.'.format(numpy.median(i_to_i), numpy.median(e_to_i), numpy.median(i_to_e))
 
-	# print 'The i-to-i, e-to-i, and i-to-e medians are {}, {}, {}.'.format(numpy.median(i_to_i), numpy.median(e_to_i), numpy.median(i_to_e))
+			if weight_type == 'mention-retweet':
+				i_to_i = i_to_i[i_to_i != 0]
+				e_to_i = e_to_i[e_to_i != 0]
+				i_to_e = i_to_e[i_to_e != 0]
 
-	if weight_type == 'mention-retweet':
-		i_to_i = i_to_i[i_to_i != 0]
-		e_to_i = e_to_i[e_to_i != 0]
-		i_to_e = i_to_e[i_to_e != 0]
+			med_i_to_i = numpy.median(i_to_i)
+			med_e_to_i = numpy.median(e_to_i)
+			med_i_to_e = numpy.median(i_to_e)
 
-	med_i_to_i = numpy.median(i_to_i)
-	med_e_to_i = numpy.median(e_to_i)
-	med_i_to_e = numpy.median(i_to_e)
+			# print med_i_to_i, med_e_to_i, med_i_to_e
 
-	# print med_i_to_i, med_e_to_i, med_i_to_e
+			rat_e_to_i = med_i_to_i/med_e_to_i
+			rat_i_to_e = med_i_to_i/med_i_to_e
 
-	rat_e_to_i = med_i_to_i/med_e_to_i
-	rat_i_to_e = med_i_to_i/med_i_to_e
+			print '{}\t{}'.format(rat_e_to_i, rat_i_to_e)
 
-	print '{}\t{}'.format(rat_e_to_i, rat_i_to_e)
+			ratio_e_to_i.append(rat_e_to_i)
+			ratio_i_to_e.append(rat_i_to_e)
 
-	ratio_e_to_i.append(rat_e_to_i)
-	ratio_i_to_e.append(rat_i_to_e)
-
-print 'The median e-to-i ratio is {} ({}).\nThe median i-to-e ratio is {} ({}).'.format(numpy.median(ratio_e_to_i), numpy.mean(ratio_e_to_i), numpy.median(ratio_i_to_e), numpy.mean(ratio_i_to_e))
+		print 'The median e-to-i ratio is {} ({}).\nThe median i-to-e ratio is {} ({}).'.format(numpy.median(ratio_e_to_i), numpy.mean(ratio_e_to_i), numpy.median(ratio_i_to_e), numpy.mean(ratio_i_to_e))
