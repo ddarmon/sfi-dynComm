d9edb41d29daf0716f002d0158a00a2928a8a746
David Darmon <ddarmon@math.umd.edu>
2014-05-06 11:12:55 -0400

    Added figure with densities by edge type.
    This is to replace the distribution by edge type figure.

--- code/ecdf_distribution_by_community_type.R
+++ code/ecdf_distribution_by_community_type.R
@@ -26,7 +26,7 @@
 # weight_type = 'mention-retweet'
 
 label_types = c('struc', 'TE4', 'hashtag', 'mention-retweet')
-weight_types = c('TE4', 'hashtag', 'mention-retweet')
+# weight_types = c('TE4', 'hashtag', 'mention-retweet')
 
 # label_types = c('struc')
 # label_types = c('TE4')
@@ -34,7 +34,7 @@ weight_types = c('TE4', 'hashtag', 'mention-retweet')
 # label_types = c('mention-retweet')
 
 # weight_types = c('TE4')
-# weight_types = c('hashtag')
+weight_types = c('hashtag')
 # weight_types = c('mention-retweet')
 
 output.filename = 'stats_for_edges.dat'
@@ -109,10 +109,49 @@ main.text = sprintf('Community Type %s, Weight Type %s', label_type, weight_type
 plot(ecdf(data_inter), verticals = vertical, do.points = to.do.points, col = 'blue', ylab = expression(paste('P(', W[i*j] <= w, '| ', T == t, ')')), xlab = expression(paste('Weight, ', w)), lwd = lwd.cdf, main = '', lty = 1, xlim = xlim)
 lines(ecdf(data_intra), verticals = vertical, do.points = to.do.points, col = 'red', lwd = lwd.cdf, lty = 2)
 lines(ecdf(data_multi), verticals = vertical, do.points = to.do.points, col = 'darkmagenta', lwd = lwd.cdf, lty = 3)
-# legend('bottomright', legend = c('Intra', 'Inter', 'Multi'), col = c('red', 'green', 'blue'), lty = 1:3, lwd = 3)
+# legend('bottomright', legend = c('Intra', 'Inter', 'Multi'), col = c('blue', 'red', 'darkmagenta'), lty = 1:3, lwd = 3)
 dev.off()
+
+fs = c(density(data_inter, from = 0)$y, density(data_intra, from = 0)$y, density(data_multi, from = 0)$y)
+
+if (weight_type == 'TE4'){
+	xlim = c(0, 0.04)
+	ylim = c(0, 150)
+	log  = ''
+} else if (weight_type == 'hashtag'){
+	xlim = c(0, 0.02)
+	# ylim = c(0, max(fs))
+	ylim = c(0, 2000)
+	log  = ''
+} else if (weight_type == 'mention-retweet'){
+	xlim = c(0, 0.1)
+	ylim = c(0, 80)
+	log = ''
+}
+
+tiff(paste0('figures/', prefix.pdf, '-pdf_by_type.tiff'), res = 120)
+par(mar=c(5,5,2,1), cex.lab = 2, cex.axis = 2)
+plot(density(data_inter, from = 0), col = 'blue', xlim = xlim, ylim = ylim, lty = 1, lwd = lwd.cdf, main = '', xlab = expression(paste('Weight, ', w)), ylab = expression(paste('f(', w, '| ', T == t, ')')), log = log)
+lines(density(data_intra, from = 0), col = 'red', lty = 2, lwd = lwd.cdf)
+lines(density(data_multi, from = 0), col = 'darkmagenta', lty = 3, lwd = lwd.cdf)
+# legend('topright', legend = c('Intra', 'Inter', 'Multi'), col = c('blue', 'red', 'darkmagenta'), lty = 1:3, lwd = 3)
+dev.off()
+
+# plot(density(data_inter, from = 0), col = 'blue', lty = 1, lwd = lwd.cdf, main = '', xlab = expression(paste('Weight, ', w)), ylab = expression(paste('f(', w, '| ', T == t, ')')))
+# lines(density(data_intra, from = 0), col = 'red', lty = 2, lwd = lwd.cdf)
+# lines(density(data_multi, from = 0), col = 'darkmagenta', lty = 3, lwd = lwd.cdf)
+# legend('topright', legend = c('Intra', 'Inter', 'Multi'), col = c('blue', 'red', 'darkmagenta'), lty = 1:3, lwd = 3)
+
+# dev.off()
 
 cat(sprintf('%s\t%s\t%f\t%f\t%f\t%g\t%g\t%g\n', label_type, weight_type, median(data_intra), median(data_inter), median(data_multi), length(data_intra), length(data_inter), length(data_multi)), file = output.filename, append = TRUE)
 
 }
 }
+
+# The density of transfer entropy estimator under the null hypothesis
+# of zero transfer entropy.
+
+# dens.x = seq(0, 5, by = 0.01)
+
+# plot(dens.x, dchisq(df = 2, x = dens.x), col = 'black', lwd = 3, type = 'l')
